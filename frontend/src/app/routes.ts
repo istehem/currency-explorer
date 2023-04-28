39431426a67b1249d9e4dce1c45c1c25e2489973
Ingemarsson Oskar <ingeos@akros.ch>
2023-04-28 10:14:17 +0200

    default route to 404 page

--- frontend/src/app/routes.ts
+++ frontend/src/app/routes.ts
@@ -1,9 +1,8 @@
 import { Routes } from "@angular/router";
+import { CurrencyDetailsComponent } from "./currency/currency-details/currency-details.component";
 import { CurrencyFavoritesComponent } from "./currency/currency-favorites/currency-favorites.component";
 import { CurrencyViewComponent } from "./currency/currency-view/currency-view.component";
-import { CurrencyDetailsComponent } from "./currency/currency-details/currency-details.component";
 import { NotFoundComponentComponent } from "./not-found-component/not-found-component.component";
-import { httpError } from "./currency.history.actions";
 import { ServerErrorComponentComponent } from "./server-error-component/server-error-component.component";
 
 export const appRoutes: Routes = [
@@ -20,9 +19,12 @@ export const appRoutes: Routes = [
         path: 'select', component: CurrencyViewComponent
     },
     {
+        path: '500', component: ServerErrorComponentComponent
+    },
+    {
         path: '404', component: NotFoundComponentComponent
     },
     {
-        path: '**', component: ServerErrorComponentComponent
+        path: '**', component: NotFoundComponentComponent
     }
 ]
